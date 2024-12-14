3a079ee2d112b1839bd83d02af71dfbb4844ec2e
Yahya S <yahyasyabani@gmail.com>
2024-12-15 00:56:59 +0700

    footer structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
 
-function Footer() {
-  return <div>Footer</div>;
-}
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <h3>E-Commerce</h3>
+          <p>Your one stop commerce</p>
+        </div>
+        <div>
+          <h4>Quick Links</h4>
+          <ul>
+            <li>
+              <Link>Home</Link>
+            </li>
+            <li>
+              <Link>Shop</Link>
+            </li>
+            <li>
+              <Link>Contact</Link>
+            </li>
+            <li>
+              <Link>About</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h4>Follow Us</h4>
+          <div>
+            <a href="">
+              <FaFacebook />
+            </a>
+            <a href="">
+              <FaTwitter />
+            </a>
+            <a href="">
+              <FaGithub />
+            </a>
+            <a href="">
+              <FaLinkedin />
+            </a>
+          </div>
+          <form>
+            <input type="email" />
+            <button>Subscribe</button>
+          </form>
+        </div>
+        <div></div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
