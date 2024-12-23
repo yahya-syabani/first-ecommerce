cc27fc050ec3bc3884435f3dbfda1e9bf65abd4e
Yahya S <yahyasyabani@gmail.com>
2024-12-23 23:28:58 +0700

    update

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -6,7 +6,7 @@ const Shop = () => {
   const products = useSelector((state) => state.products);
   return (
     <div className="mx-auto px-4 py-12 md:px-16 lg:px-24">
-      <h2 className="mb-6 text-center text-2xl font-bold">Shop</h2>
+      <h2 className="mb-6 text-center text-2xl font-bold">Update</h2>
       <div className="grid cursor-pointer grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
         {products.products.map((product) => (
           <ProductCard product={product} />
